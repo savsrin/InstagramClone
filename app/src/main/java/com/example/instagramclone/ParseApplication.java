b1199b776ccc5d542aeb33f0a550dd2f15781c37
savsrin <savsrin@gmail.com>
2021-07-08 12:01:29 -0700

    added posting photos feature

--- app/src/main/java/com/example/instagramclone/ParseApplication.java
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -3,11 +3,14 @@ package com.example.instagramclone;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("kKvnZRoo4Gl7mkMAvY2uuwruAMsktVhU8BfygVQ7")
                 .clientKey("JEbNJW3AEX7wyaP1bDsNdTtyUpQX4dKwqqftX5KB")
